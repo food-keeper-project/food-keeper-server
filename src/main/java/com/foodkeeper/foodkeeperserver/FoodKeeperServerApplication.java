5dca6ac92fc352a1de2e2866bca93e6c0dd04818
rt3310 <btac3310@gmail.com>
2025-12-08 02:40:15 +0900

    initialize project

---
+++ src/main/java/com/foodkeeper/foodkeeperserver/FoodKeeperServerApplication.java
@@ -0,0 +1,13 @@
+package com.foodkeeper.foodkeeperserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodKeeperServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodKeeperServerApplication.class, args);
+    }
+
+}
