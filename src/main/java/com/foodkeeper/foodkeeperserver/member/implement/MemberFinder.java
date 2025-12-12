c56dbcdd9867997f65a02f8b25bf2dfe1e291e98
rt3310 <btac3310@gmail.com>
2025-12-12 16:47:32 +0900

    refactor[#3]: Transactional(readOnly) 추가

--- src/main/java/com/foodkeeper/foodkeeperserver/member/implement/MemberFinder.java
+++ src/main/java/com/foodkeeper/foodkeeperserver/member/implement/MemberFinder.java
@@ -6,12 +6,14 @@ import com.foodkeeper.foodkeeperserver.support.exception.AppException;
 import com.foodkeeper.foodkeeperserver.support.exception.ErrorType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
 public class MemberFinder {
     private final MemberRepository memberRepository;
 
+    @Transactional(readOnly = true)
     public Member find(String memberKey) {
         return memberRepository.findByMemberKey(memberKey).orElseThrow(() -> new AppException(ErrorType.NOT_FOUND_DATA))
                 .toDomain();
