05bf344557af4debd00b72fedae828f6fbe22f5a
rt3310 <btac3310@gmail.com>
2025-12-13 21:53:06 +0900

    feature[#12]: 식재료 즐겨찾기 추가 기능 구현

---
+++ src/main/java/com/foodkeeper/foodkeeperserver/bookmarkedfood/dataaccess/entity/BookmarkedFoodEntity.java
@@ -0,0 +1,43 @@
+package com.foodkeeper.foodkeeperserver.bookmarkedfood.dataaccess.entity;
+
+import com.foodkeeper.foodkeeperserver.common.dataaccess.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "bookmarked_food")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BookmarkedFoodEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bookmarked_food_id")
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @Column(nullable = false, length = 20)
+    private String storageMethod;
+
+    @Column(nullable = false)
+    private String memberKey;
+
+    @Builder
+    public BookmarkedFoodEntity(String name,
+                                String imageUrl,
+                                String storageMethod,
+                                String memberKey) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.storageMethod = storageMethod;
+        this.memberKey = memberKey;
+    }
+}
---
+++ src/main/java/com/foodkeeper/foodkeeperserver/bookmarkedfood/domain/Food.java
@@ -0,0 +1,10 @@
+package com.foodkeeper.foodkeeperserver.bookmarkedfood.domain;
+
+import lombok.Getter;
+
+@Getter
+public class Food {
+    String name;
+    String imageUrl;
+    String storageMethod;
+}
