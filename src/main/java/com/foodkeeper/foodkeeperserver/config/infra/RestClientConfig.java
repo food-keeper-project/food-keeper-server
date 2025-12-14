6ad04dcf5cc25ecf7cea5d4bf7687d3d2781191e
brian <brian506@naver.com>
2025-12-14 14:46:25 +0900

    refactor[#5] : RestClientConfig 서비스별 분리

--- src/main/java/com/foodkeeper/foodkeeperserver/config/infra/RestClientConfig.java
+++ src/main/java/com/foodkeeper/foodkeeperserver/config/infra/RestClientConfig.java
@@ -10,18 +10,30 @@ import java.time.Duration;
 @Configuration
 public class RestClientConfig {
 
-    //todo 타임아웃 시간 재설정(AI,KAKAO)
-    private static final int CONNECTION_TIMEOUT_SECONDS = 5;
-    private static final int READ_TIMEOUT_SECONDS = 20;
+    private static final int KAKAO_CONNECT_TIMEOUT = 5;
+    private static final int KAKAO_READ_TIMEOUT = 3;
 
-    @Bean
-    public RestClient restClient() {
-        var factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout((int) Duration.ofSeconds(CONNECTION_TIMEOUT_SECONDS).toMillis());
-        factory.setReadTimeout((int) Duration.ofSeconds(READ_TIMEOUT_SECONDS).toMillis());
+    private static final int CLOVA_CONNECT_TIMEOUT = 5;
+    private static final int CLOVA_READ_TIMEOUT = 20;
 
+    @Bean("kakaoRestClient")
+    public RestClient kakakoRestClient() {
         return RestClient.builder()
-                .requestFactory(factory)
+                .requestFactory(createFactory(KAKAO_CONNECT_TIMEOUT,KAKAO_READ_TIMEOUT))
                 .build();
+    }
+
+    @Bean("clovaRestClient")
+    public RestClient clovaRestClient() {
+        return RestClient.builder()
+                .requestFactory(createFactory(CLOVA_CONNECT_TIMEOUT,CLOVA_READ_TIMEOUT))
+                .build();
+    }
+
+    private SimpleClientHttpRequestFactory createFactory(int connectTimeout, int readTimeout) {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout((int) Duration.ofSeconds(connectTimeout).toMillis());
+        factory.setReadTimeout((int) Duration.ofSeconds(readTimeout).toMillis());
+        return factory;
     }
 }
