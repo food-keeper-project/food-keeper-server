256583f10d167d47e233dbaedf14e3dabff9a59b
rt3310 <btac3310@gmail.com>
2025-12-15 15:34:24 +0900

    docs[#13]: 카카오 로그인 swagger doc 작성

--- src/main/java/com/foodkeeper/foodkeeperserver/auth/controller/v1/SignInController.java
+++ src/main/java/com/foodkeeper/foodkeeperserver/auth/controller/v1/SignInController.java
@@ -7,6 +7,8 @@ import com.foodkeeper.foodkeeperserver.auth.domain.Jwt;
 import com.foodkeeper.foodkeeperserver.auth.domain.MemberRegister;
 import com.foodkeeper.foodkeeperserver.member.domain.enums.OAuthProvider;
 import com.foodkeeper.foodkeeperserver.support.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Auth", description = "인증 관련 API")
 @RestController
 @RequestMapping("/api/v1/sign-in")
 @RequiredArgsConstructor
@@ -23,6 +26,7 @@ public class SignInController {
 
     private final SignInService signinService;
 
+    @Operation(summary = "카카오 로그인", description = "카카오 로그인 API")
     @PostMapping("/kakao")
     public ResponseEntity<ApiResponse<AuthTokenResponse>> signInWithKakao(
             @Valid @RequestBody SignInRequest signInRequest, HttpServletRequest request) {
