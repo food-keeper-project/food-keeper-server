91a03fc61998368e0ae207dbe26d7bd59ff95940
rt3310 <btac3310@gmail.com>
2025-12-15 18:14:12 +0900

    feature[#13]: JSpecify Null safety annotation 추가

--- src/main/java/com/foodkeeper/foodkeeperserver/auth/dataaccess/repository/MemberRoleRepository.java
+++ src/main/java/com/foodkeeper/foodkeeperserver/auth/dataaccess/repository/MemberRoleRepository.java
@@ -1,10 +1,12 @@
 package com.foodkeeper.foodkeeperserver.auth.dataaccess.repository;
 
 import com.foodkeeper.foodkeeperserver.auth.dataaccess.entity.MemberRoleEntity;
+import org.jspecify.annotations.NullMarked;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+@NullMarked
 public interface MemberRoleRepository extends JpaRepository<MemberRoleEntity, Long> {
     List<MemberRoleEntity> findByMemberKey(String memberKey);
 }
--- src/main/java/com/foodkeeper/foodkeeperserver/member/dataaccess/repository/MemberRepository.java
+++ src/main/java/com/foodkeeper/foodkeeperserver/member/dataaccess/repository/MemberRepository.java
@@ -1,10 +1,12 @@
 package com.foodkeeper.foodkeeperserver.member.dataaccess.repository;
 
 import com.foodkeeper.foodkeeperserver.member.dataaccess.entity.MemberEntity;
+import org.jspecify.annotations.NullMarked;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+@NullMarked
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
     Optional<MemberEntity> findByMemberKey(String memberKey);
 }
