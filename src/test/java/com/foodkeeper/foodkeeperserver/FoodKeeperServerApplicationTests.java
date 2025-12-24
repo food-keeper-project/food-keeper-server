88be0d184bb1fd7ec885f15fb1c6ec52bed34689
rt3310 <btac3310@gmail.com>
2025-12-24 19:13:12 +0900

    fix[#38]: local, test 환경 분리

--- src/test/java/com/foodkeeper/foodkeeperserver/FoodKeeperServerApplicationTests.java
+++ src/test/java/com/foodkeeper/foodkeeperserver/FoodKeeperServerApplicationTests.java
@@ -1,9 +1,7 @@
 package com.foodkeeper.foodkeeperserver;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class FoodKeeperServerApplicationTests {
 
     @Test
