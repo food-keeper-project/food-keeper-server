857b85e2a05f2d2411e9bdc735e69db4b7be0120
brian <brian506@naver.com>
2026-01-01 19:48:19 +0900

    refactor[#71]: 병합 충돌 해결

--- src/main/java/com/foodkeeper/foodkeeperserver/food/controller/v1/FoodController.java
+++ src/main/java/com/foodkeeper/foodkeeperserver/food/controller/v1/FoodController.java
@@ -7,9 +7,9 @@ import com.foodkeeper.foodkeeperserver.food.business.FoodService;
 import com.foodkeeper.foodkeeperserver.food.controller.v1.request.FoodRegisterRequest;
 import com.foodkeeper.foodkeeperserver.food.controller.v1.request.FoodUpdateRequest;
 import com.foodkeeper.foodkeeperserver.food.controller.v1.request.FoodsRequest;
-import com.foodkeeper.foodkeeperserver.food.controller.v1.response.FoodResponses;
 import com.foodkeeper.foodkeeperserver.food.controller.v1.response.FoodIdResponse;
 import com.foodkeeper.foodkeeperserver.food.controller.v1.response.FoodResponse;
+import com.foodkeeper.foodkeeperserver.food.controller.v1.response.FoodResponses;
 import com.foodkeeper.foodkeeperserver.food.domain.RegisteredFood;
 import com.foodkeeper.foodkeeperserver.food.domain.request.FoodRegister;
 import com.foodkeeper.foodkeeperserver.member.domain.Member;
@@ -26,9 +26,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.net.URI;
-import java.time.LocalDateTime;
-import java.util.List;
-
 import java.util.List;
 
 @Tag(name = "Food", description = "식재료 관련 API")
@@ -43,8 +40,8 @@ public class FoodController {
     @Operation(summary = "식재료 추가", description = "식재료 추가 API")
     @PostMapping
     public ResponseEntity<ApiResponse<Void>> createFood(@RequestPart @Valid FoodRegisterRequest request,
-                                                                  @RequestPart(required = false) MultipartFile image,
-                                                                  @AuthMember Member authMember) {
+                                                        @RequestPart(required = false) MultipartFile image,
+                                                        @AuthMember Member authMember) {
         FoodRegister register = FoodRegisterRequest.toRegister(request);
         Long foodId = foodService.registerFood(register, image, authMember.memberKey());
         return ResponseEntity.created(URI.create("/api/v1/foods" + foodId)).build();
@@ -62,8 +59,8 @@ public class FoodController {
     @Operation(summary = "식재료 전체 조회", description = "식재료 전체 조회 API")
     @GetMapping
     public ResponseEntity<ApiResponse<PageResponse<FoodResponse>>> findFoods(@ModelAttribute FoodsRequest request,
-                                                                            @CursorDefault Cursorable<Long> cursorable,
-                                                                            @AuthMember Member authMember) {
+                                                                             @CursorDefault Cursorable<Long> cursorable,
+                                                                             @AuthMember Member authMember) {
         SliceObject<RegisteredFood> foods = foodService.findFoodList(cursorable, request.categoryId(), authMember.memberKey());
         List<FoodResponse> foodResponses = foods.content().stream().map(FoodResponse::toFoodResponse).toList();
         return ResponseEntity.ok(ApiResponse.success(new PageResponse<>(foodResponses, foods.hasNext())));
@@ -100,16 +97,17 @@ public class FoodController {
         Long resultId = foodService.removeFood(foodId, member.memberKey());
         return ResponseEntity.created(URI.create("/api/v1/foods" + resultId)).build();
     }
+
     @NullMarked
     @Operation(summary = "식재료 수정", description = "식재료 수정 API")
     @PatchMapping("/{foodId}")
-    public ResponseEntity<ApiResponse<FoodIdResponse>> updateFood(@PathVariable Long foodId,
-                                                                  @RequestPart @Valid FoodUpdateRequest request,
-                                                                  @RequestPart(required = false) MultipartFile image,
-                                                                  @AuthMember Member authMember) {
+    public ResponseEntity<ApiResponse<Void>> updateFood(@PathVariable Long foodId,
+                                                        @RequestPart @Valid FoodUpdateRequest request,
+                                                        @RequestPart(required = false) MultipartFile image,
+                                                        @AuthMember Member authMember) {
         FoodRegister foodRegister = FoodUpdateRequest.toRegister(request);
         Long resultId = foodService.updateFood(foodId, foodRegister, image, authMember.memberKey());
-        return ResponseEntity.ok(ApiResponse.success(new FoodIdResponse(resultId)));
+        return ResponseEntity.created(URI.create("/api/v1/foods" + resultId)).build();
     }
 
 }
