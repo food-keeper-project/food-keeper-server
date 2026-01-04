5aea472cc5c24d376db992ea959b851996d116c0
rt3310 <btac3310@gmail.com>
2026-01-05 01:08:43 +0900

    test[#141]: 식재료 리스트 페이지 조회 테스트 코드 작성

--- src/test/java/com/foodkeeper/foodkeeperserver/food/e2e/FoodE2ETest.java
+++ src/test/java/com/foodkeeper/foodkeeperserver/food/e2e/FoodE2ETest.java
@@ -1,7 +1,14 @@
 package com.foodkeeper.foodkeeperserver.food.e2e;
 
 import com.foodkeeper.foodkeeperserver.food.dataaccess.entity.FoodCategoryEntity;
+import com.foodkeeper.foodkeeperserver.food.dataaccess.entity.FoodEntity;
+import com.foodkeeper.foodkeeperserver.food.dataaccess.entity.SelectedFoodCategoryEntity;
 import com.foodkeeper.foodkeeperserver.food.dataaccess.repository.FoodCategoryRepository;
+import com.foodkeeper.foodkeeperserver.food.dataaccess.repository.FoodRepository;
+import com.foodkeeper.foodkeeperserver.food.dataaccess.repository.SelectedFoodCategoryRepository;
+import com.foodkeeper.foodkeeperserver.food.domain.SelectedFoodCategory;
+import com.foodkeeper.foodkeeperserver.food.domain.StorageMethod;
+import com.foodkeeper.foodkeeperserver.food.domain.request.FoodRegister;
 import com.foodkeeper.foodkeeperserver.member.dataaccess.entity.MemberEntity;
 import com.foodkeeper.foodkeeperserver.member.dataaccess.repository.MemberRepository;
 import com.foodkeeper.foodkeeperserver.member.fixture.MemberEntityFixture;
@@ -12,10 +19,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.MultipartBodyBuilder;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class FoodE2ETest extends E2ETest {
 
-    @Autowired MemberRepository memberRepository;
-    @Autowired FoodCategoryRepository foodCategoryRepository;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    FoodCategoryRepository foodCategoryRepository;
+    @Autowired
+    SelectedFoodCategoryRepository selectedFoodCategoryRepository;
+    @Autowired
+    FoodRepository foodRepository;
 
     @Test
     @DisplayName("식재료를 저장한다.")
@@ -130,5 +146,47 @@ public class FoodE2ETest extends E2ETest {
                 .body(builder.build())
                 .exchange()
                 .expectStatus().isBadRequest();
+    }
+
+    @Test
+    @DisplayName("식재료 리스트 페이지를 조회한다.")
+    void findFoodPage() {
+        MemberEntity member = memberRepository.save(MemberEntityFixture.DEFAULT.get());
+        FoodCategoryEntity category = foodCategoryRepository.save(
+                FoodCategoryEntity.builder().name("test").memberKey(member.getMemberKey()).build());
+        List<LocalDate> localDates = List.of(
+                LocalDate.of(2026, 1, 2),
+                LocalDate.of(2026, 1, 1),
+                LocalDate.of(2026, 1, 5),
+                LocalDate.of(2026, 1, 2),
+                LocalDate.of(2026, 1, 7),
+                LocalDate.of(2026, 1, 1),
+                LocalDate.of(2026, 1, 3),
+                LocalDate.of(2026, 1, 5),
+                LocalDate.of(2026, 1, 10),
+                LocalDate.of(2026, 1, 3),
+                LocalDate.of(2026, 1, 7),
+                LocalDate.of(2026, 1, 9),
+                LocalDate.of(2026, 1, 1)
+        );
+
+        localDates.forEach(localDate -> {
+            FoodRegister foodRegister = new FoodRegister("test", List.of(category.getId()), StorageMethod.FROZEN, localDate, 2, "");
+            FoodEntity food = foodRepository.save(FoodEntity.from(foodRegister, "https://test.com/image.jpg", member.getMemberKey()));
+            selectedFoodCategoryRepository.save(SelectedFoodCategoryEntity.from(new SelectedFoodCategory(null, food.getId(), category.getId())));
+        });
+
+        client.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/api/v1/foods")
+                        .queryParam("categoryId", category.getId())
+                        .queryParam("limit", 10)
+                        .build())
+                .header(AUTHORIZATION, getAccessToken(member.getMemberKey()))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .consumeWith(System.out::println);
+
     }
 }
