785b90d50be7c539fe1c6ee9957425e121bb1a3e
rt3310 <btac3310@gmail.com>
2026-01-10 18:04:56 +0900

    hotfix[#181]: 회원가입 인증 api 허용 처리

--- src/main/java/com/foodkeeper/foodkeeperserver/support/ratelimit/filter/ApiThrottlingFilter.java
+++ src/main/java/com/foodkeeper/foodkeeperserver/support/ratelimit/filter/ApiThrottlingFilter.java
@@ -31,9 +31,9 @@ import java.util.List;
 public class ApiThrottlingFilter implements Filter {
     private static final List<LimitApi> LIMIT_APIS =
             List.of(
-//                    LimitApi.pattern("/api/v1/auth/sign-in/**"),
-//                    LimitApi.pattern("POST", "/api/v1/auth/check/account"),
-//                    LimitApi.pattern("POST", "/api/v1/auth/verify/email")
+                    LimitApi.pattern("/api/v1/auth/sign-in/**"),
+                    LimitApi.pattern("POST", "/api/v1/auth/check/account"),
+                    LimitApi.pattern("POST", "/api/v1/auth/verify/email")
             );
     public static final int ONE_SECOND_TO_NANOS = 1_000_000_000;
 
